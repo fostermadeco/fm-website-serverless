4d8b3185d4acba8656b98378172ee83e5649a512
Kelly Packer <kelly@kellypacker.com>
2022-02-18 13:08:33 -0700

    Add cloudfront

--- handler.js
+++ handler.js
@@ -2,7 +2,6 @@
 
 const AWS = require('aws-sdk');
 
-
 module.exports.requestUploadURL = async (event, context, callback) => {
   const s3 = new AWS.S3();
   const params = JSON.parse(event.body);
@@ -11,7 +10,7 @@ module.exports.requestUploadURL = async (event, context, callback) => {
     Bucket: 'fm-website-apply-docs',
     Key:  params.name,
     ContentType: params.type,
-    ACL: 'public-read',
+    ACL: 'private',
   };
   
   const uploadURL = s3.getSignedUrl('putObject', s3Params);
@@ -19,9 +18,40 @@ module.exports.requestUploadURL = async (event, context, callback) => {
   callback(null, {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': 'https://happy-joliot-c09cdf.netlify.app',
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({ uploadURL: uploadURL }),
   });
 
 };
+
+// function managed in AWS, not here, but for reference
+// module.exports.authAccess = async (event, context, callback) => {
+//   // Get request and request headers
+//   const request = event.Records[0].cf.request;
+//   const headers = request.headers;
+
+//   // Configure authentication
+//   const authUser = 'USER';
+//   const authPass = 'PASS';
+
+//   // Construct the Basic Auth string
+//   const authString = 'Basic ' + new Buffer(authUser + ':' + authPass).toString('base64');
+
+//   // Require Basic authentication
+//   if (typeof headers.authorization == 'undefined' || headers.authorization[0].value != authString) {
+//       const body = 'Unauthorized';
+//       const response = {
+//           status: '401',
+//           statusDescription: 'Unauthorized',
+//           body: body,
+//           headers: {
+//               'www-authenticate': [{key: 'WWW-Authenticate', value:'Basic'}]
+//           },
+//       };
+//       callback(null, response);
+//   }
+
+//   // Continue request processing if authentication passed
+//   callback(null, request);
+// };
